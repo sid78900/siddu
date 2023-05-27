e28429bc4cb13c030dfeb83790f9f646dbeed901
Siddharthgasti7 <35592022+Siddharthgasti7@users.noreply.github.com>
2023-05-27 17:56:37 +0530

    newww

--- Git/src/gitpackage/testclass.java
+++ Git/src/gitpackage/testclass.java
@@ -3,8 +3,6 @@ package gitpackage;
 public class testclass {
 public static void main(String[] args) {
 	System.out.println("siddu");
-	System.out.println("hii");
-	System.out.println("new branch");
-	System.out.println("sh12");
+	
 }
 }
